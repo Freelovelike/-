512e28724083689cb930180ae6e4f92f3152fb92
廖保发 <1046650845@qq.com>
2023-11-10 20:04:43 +0800

    注册用户接口

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,10 +1,33 @@
 /**
 *在这里定义和用户相关的路由处理函数，供/router/user.js 模块进行调用*/
 
+// 导入数据库模块
+const db=require('../db/index.js')
 
 //注册用户的处理函数
 exports.regUser = function(req, res){
-    res.send('注册用户')
+    // 获取客户端提交到服务器的用户信息
+    const userInfo=req.body
+    if(!userInfo.username || !userInfo.password){
+        return res.send({code:400,msg:'用户名或密码不能为空'})
+    }
+    // 定义SQL语句，查询用户名是否被占用
+    const sqlStr='select * from ev_users where username=?'
+    db.query(sqlStr,[userInfo.username],(err,results)=>{
+            if(err) return res.send({code:500,msg:'数据库查询错误'})
+            if(results.length>0){
+                return res.send({code:400,msg:'用户名已存在'})
+            }
+            // 执行其他操作--将输入插入数据库
+            const insertSql='insert into ev_users set ?'
+            db.query(insertSql,userInfo,(err,results)=>{
+                if(err) return res.send({code:500,msg:'数据库插入错误'})
+                if(results.affectedRows>0){
+                    return res.send({code:200,msg:'注册成功'})
+                }
+            })
+        }
+    )
 }
 
 //登录的处理函数
