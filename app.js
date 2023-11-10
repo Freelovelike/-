af0b49718f94e5894b3059fcea53f2b70e637e58
廖保发 <1046650845@qq.com>
2023-11-10 18:05:15 +0800

    配置cors跨域，配置解析表单提交格式

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const express = require('express')
 // 创建服务器的实例对象
 const app = express()
 
+// 导入CORS中间件
+const cors = require('cors')
+// 将CORS注册为全局中间件
+app.use(cors())
+ 
+// 配置解析表单数据的中间件 注意：这个中间件，只能解析application/x-www-form-urlencoded 格式的表单
+app.use(express.urlencoded({ extended: false }))
+// 配置解析JSON格式的中间件
+app.use(express.json())
+
 // 启动服务器
 app.listen(80, () => {
-    console.log('服务器启动成功')       
+    console.log('服务器启动成功,http://127.0.0.1:80')       
 })
