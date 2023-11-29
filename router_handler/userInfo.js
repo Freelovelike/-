d829ada38f68fe52d8e55784715a28d40f933d51
廖保发 <1046650845@qq.com>
2023-11-29 21:53:17 +0800

    修改获取用户信息返回的报错

--- router_handler/userInfo.js
+++ router_handler/userInfo.js
@@ -22,13 +22,15 @@ exports.getUserInfo=(req,res)=>{
 // 根据用户名获取当前用户信息
 exports.getUserInfo = function(req, res){
     // 获取客户端提交到服务器的用户信息
-    const userInfo=req.body
+    const userInfo=req.query
+    console.log(req,'userInfo');
     // 定义SQL语句
     const sqlStr='select * from ev_users where username=?'
     db.query(sqlStr,[userInfo.username],(err,results)=>{
         if(err) return res.cc(500,'数据库查询错误')
+        console.log(results,'results');
         if(results.length!==1){
-            return res.cc(400,'用户名或密码错误')
+            return res.cc(400,'用户名或Token错误')
         }
         // 调用res.send()将Token响应给客户端
         res.send({code:200,msg:'获取成功',data:results[0]})
